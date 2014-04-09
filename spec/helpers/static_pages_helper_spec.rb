30017d9dfe66d634614fb819801e422f99033f22
Andrew <andrewlee5889@yahoo.com>
2014-04-09 08:17:58 -0700

    Finish sign in

--- spec/helpers/static_pages_helper_spec.rb
+++ spec/helpers/static_pages_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
-# the StaticPagesHelper. For example:
+# the StaticPageHelper. For example:
 #
-# describe StaticPagesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
+# describe StaticPageHelper do
+# describe "string concat" do
+# it "concats two strings with spaces" do
+# helper.concat_strings("this","that").should == "this that"
 # end
-describe StaticPagesHelper do
+# end
+# end
+describe StaticPageHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
